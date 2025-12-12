d3ddeca1420200910e19833fa1a00a3364b62b4c
24051687AnushkaBarat <24051687@kiit.ac.in>
2025-12-12 13:26:03 +0530

    ques 4

---
+++ src/palin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class palin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);1
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num;
+        int reversed = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+        if (original == reversed) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+        }
+}
