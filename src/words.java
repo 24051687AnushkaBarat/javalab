6ab796c3086c88d6ee95477c2d0e1c610ab8ddba
24051687AnushkaBarat <24051687@kiit.ac.in>
2025-12-12 13:42:50 +0530

    ques 5

---
+++ src/words.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class words {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.println("reverse order " + lastName + " " + firstName);
+    }
+}
